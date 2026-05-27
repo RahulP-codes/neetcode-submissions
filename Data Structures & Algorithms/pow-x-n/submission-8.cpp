017e14dd679b65da8ed1274e97690ad92c8c017a
RahulP-codes <rahul7pol7@gmail.com>
2026-05-27 17:20:12 +0530

    Add: pow-x-n - submission-8

---
+++ Data Structures & Algorithms/pow-x-n/submission-8.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double res = 1;
+        for (int i=0; i<abs(n); i++) {
+            res *= x;
+        }
+
+        return n>=0 ? res : 1/res;
+    }
+};
