9b701a366a91c9dd5285f23aad8803c253345402
RahulP-codes <rahul7pol7@gmail.com>
2026-06-03 12:08:27 +0530

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        vector<int> count(3, 0);
+
+        for (int num:nums) {
+            count[num]++;
+        }
+
+        for (int i = 0; i<nums.size(); i++) {
+            if (i < count[0]) {
+                nums[i] = 0;
+            } else if (i < count[1]+count[0]) {
+                nums[i] = 1;
+            } else {
+                nums[i] = 2;
+            }
+        }
+    }
+};
