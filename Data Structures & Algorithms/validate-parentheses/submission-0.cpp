21deaa4f747cca124231d7bab343419d77dc1934
RahulP-codes <rahul7pol7@gmail.com>
2026-06-06 15:34:34 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char c:s) {
+            if(c=='(' || c=='{' || c=='[') {
+                st.push(c);
+            } else if (st.empty()) return false;
+            else {
+                char t = st.top();
+                if ((t=='(' && c==')') || 
+                    (t=='{' && c=='}') ||
+                    (t=='[' && c==']')) {
+                        st.pop();
+                    }
+                else return false;
+            }
+        }
+
+        return st.empty();
+    }
+};
