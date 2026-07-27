f5ec62248f1bafccc740d3335f8c11f524fd988f
RahulP-codes <rahul7pol7@gmail.com>
2026-07-28 01:12:20 +0530

    Add: count-subsequences - submission-4

---
+++ Data Structures & Algorithms/count-subsequences/submission-4.py
@@ -0,0 +1,26 @@
+from functools import cache
+
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        slen = len(s)
+        tlen = len(t)
+
+        memo = {}
+
+        def df(i, j):
+            if j >= tlen:
+                return 1
+            if i >= slen:
+                return 0
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            if s[i] == t[j]:
+                memo[(i,j)] = df(i+1, j+1) + df(i+1, j)
+                return df(i+1, j+1) + df(i+1, j)
+
+            memo[(i, j)] = df(i+1, j)
+            return df(i+1, j)
+
+        return df(0,0)
