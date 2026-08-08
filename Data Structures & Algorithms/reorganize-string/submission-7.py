21b9d8936a63ef4c9d5c5a30389e095960e0ebe1
RahulP-codes <rahul7pol7@gmail.com>
2026-08-08 20:06:45 +0530

    Add: reorganize-string - submission-7

---
+++ Data Structures & Algorithms/reorganize-string/submission-7.py
@@ -0,0 +1,37 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        n = len(s)
+        
+        mp = {}
+        for c in s:
+            if not c in mp:
+                mp[c] = 0
+            mp[c] += 1
+
+            if mp[c] > (n+1)//2:
+                return ""
+
+        chars = []
+        for p in mp:
+            chars.append([-mp[p], p])
+
+        heapq.heapify(chars)
+
+        print(chars)
+
+        res = []
+        ban = None
+
+        i = 0
+        while chars:
+            res.append(chars[0][1])
+            chars[0][0] += 1
+            temp = chars[0]
+            heapq.heappop(chars)
+
+            if ban and ban[0] < 0:
+                heapq.heappush(chars, ban)
+
+            ban = temp
+
+        return "".join(res)
