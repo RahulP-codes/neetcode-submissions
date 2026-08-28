e617bf370d3440fcbd4a59b804632e031c7d3734
RahulP-codes <rahul7pol7@gmail.com>
2026-08-28 17:32:00 +0530

    Add: integer-break - submission-3

---
+++ Data Structures & Algorithms/integer-break/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n == 2:
+            return 1
+        elif n == 3:
+            return 2
+
+        dp = [1] * (n+1)
+        dp[2] = 2
+        dp[3] = 3
+
+        n_3 = 1
+        n_2 = 2
+        n_1 = 3
+
+        for num in range(4, n+1):
+            n_1, n_2, n_3 = max(2*n_2, 3*n_3), n_1, n_2
+
+        return n_1
